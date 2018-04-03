2fecf709b93b11593da153fcf2733a04610cd112
lazarcl <lazarcl@whitman.edu>
2018-04-03 09:36:29 -0700

    changed Market.js to Exchange.js

---
+++ frontend/src/Exchange.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Page from './Page';
+
+class Exchange extends Component {
+
+  render() {
+    return (
+      <Page>
+	      <h1>Exchange</h1>
+	  </Page>
+    );
+  }
+}
+
+export default Exchange;
